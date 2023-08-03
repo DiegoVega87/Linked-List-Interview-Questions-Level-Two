66f615f9e14de38ece335d64009cacc16d8e30c1
Diego Vega <diegovega@me.com>
2023-08-03 12:20:05 +0200

    Add problem explanation

---
+++ src/LinkedListIntersection.java
@@ -0,0 +1,10 @@
+public class LinkedListIntersection {
+
+    /*
+    * In our startup's inventory management system, we have two linked-lists representing the items in our warehouses.
+    * We need to find the node at which the two lists intersect in order to track the movement of items between
+    * warehouses. Can you write a function that takes in the heads of the two linked-lists and returns the intersecting
+    *  node or None if there is none?
+    *
+    * */
+}
