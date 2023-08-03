90f58d210af7d12a7f528d3b3ef7c544f73cf9f4
Diego Vega <diegovega@me.com>
2023-08-03 12:25:49 +0200

    Add problem examples

--- src/LinkedListIntersection.java
+++ src/LinkedListIntersection.java
@@ -6,5 +6,33 @@ public class LinkedListIntersection {
     * warehouses. Can you write a function that takes in the heads of the two linked-lists and returns the intersecting
     *  node or None if there is none?
     *
-    * */
+    * Example 1:
+    *   ListNode headA = new ListNode(4);
+    *   headA.next = new ListNode(1);
+    *   headA.next.next = new ListNode(8);
+    *   headA.next.next.next = new ListNode(4);
+    *   headA.next.next.next.next = new ListNode(5);
+    *
+    *   ListNode headB = new ListNode(5);
+    *   headB.next = new ListNode(0);
+    *   headB.next.next = new ListNode(1);
+    *   headB.next.next.next = new ListNode(8);
+    *   headB.next.next.next.next = new ListNode(4);
+    *   headB.next.next.next.next.next = new ListNode(5);
+    *
+    *   Output ListNode intersectingNode = new ListNode(8);
+    *   intersectingNode.next = new ListNode(4);
+    *   intersectingNode.next.next = new ListNode(5);
+    *
+    * Example 2:
+    *   ListNode headA = new ListNode(1);
+    *   headA.next = new ListNode(2);
+    *   headA.next.next = new ListNode(3);
+    *
+    *   ListNode headB = new ListNode(4);
+    *   headB.next = new ListNode(5);
+    *   headB.next.next = new ListNode(6);
+    *
+    *   Output: ListNode intersectingNode = null;
+     * */
 }
