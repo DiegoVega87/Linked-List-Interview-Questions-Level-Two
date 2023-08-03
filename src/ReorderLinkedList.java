ae4b91603a041f107f7fac7322262045c8eb502a
Diego Vega <diegovega@me.com>
2023-08-03 11:11:08 +0200

    Add problem explanation

---
+++ src/ReorderLinkedList.java
@@ -0,0 +1,12 @@
+public class ReorderLinkedList {
+
+    /*
+    * Our startup is designing a fitness tracking app that records users' daily workouts. We need to group all the daily
+    * workout data in a linked list. However, we want to reorder the linked list by first listing all the workout data
+    * with odd indices and then the workout data with even indices. Can you help us design a function that takes the
+    * head of the linked list as input and returns the reordered linked list? The first workout data will be considered
+    * odd, the second even, the third odd continuing with the same pattern...
+    *
+    *
+     * */
+}
