94b6af02cda1f82c662e5a6780d75354d63556ee
Diego Vega <diegovega@me.com>
2023-08-03 10:48:18 +0200

    Add examples

--- src/LinkedListAddition.java
+++ src/LinkedListAddition.java
@@ -5,5 +5,17 @@ public class LinkedListAddition {
     * reverse order. We need you to write a function that takes in these two linked lists and returns their sum as
     * another linked list in the same format.
     *
+    * Example 1:
+    *   l1: 2 -> 4 -> 3
+    *   l2: 5 -> 6 -> 4
+    *   Output: 7 -> 0 -> 8
+    *   Explanation: 342 + 465 = 807
+    *
+    * Example 2:
+    *   l1: 9 -> 9 -> 9 -> 9
+    *   l2: 9 -> 9 -> 9
+    *   Output: 8 -> 9 -> 9 -> 0 -> 1
+    *   Explanation: 9999 + 999 = 10998
+    *
     * */
 }
