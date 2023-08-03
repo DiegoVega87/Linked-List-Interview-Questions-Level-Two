a06cca495f2a2d368408e39fa7fb5c927f4a49d0
Diego Vega <diegovega@me.com>
2023-08-03 10:28:25 +0200

    Add problem explanation

---
+++ src/LinkedListAddition.java
@@ -0,0 +1,9 @@
+public class LinkedListAddition {
+    /*
+    * As part of a startup, we are building a financial application that requires adding two non-negative numbers
+    * represented as linked lists. The two linked lists are non-empty and represent the digits of each number in
+    * reverse order. We need you to write a function that takes in these two linked lists and returns their sum as
+    * another linked list in the same format.
+    *
+    * */
+}
